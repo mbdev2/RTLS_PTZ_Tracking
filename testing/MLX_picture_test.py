e6acc1c4fdb681ca5581f926ecf2f6f7dcbaad2e
mbdev <mark.breznik@gmail.com>
2021-05-28 11:46:27 +0200

    test

--- testing/MLX_picture_test.py
+++ testing/MLX_picture_test.py
@@ -16,66 +16,15 @@ import busio
 
 import adafruit_mlx90640
 
-INTERPOLATE = 20
+INTERPOLATE = 2
 
 # MUST set I2C freq to 1MHz in /boot/config.txt
 i2c = busio.I2C(board.SCL, board.SDA)
 
 # low range of the sensor (this will be black on the screen)
-MINTEMP = 20.0
+MINTEMP = 15.0
 # high range of the sensor (this will be white on the screen)
 MAXTEMP = 40.0
-
-# the list of colors we can choose from
-heatmap = (
-    (0.0, (0, 0, 0)),
-    (0.20, (0, 0, 0.5)),
-    (0.40, (0, 0.5, 0)),
-    (0.60, (0.5, 0, 0)),
-    (0.80, (0.75, 0.75, 0)),
-    (0.90, (1.0, 0.75, 0)),
-    (1.00, (1.0, 1.0, 1.0)),
-)
-
-# how many color values we can have
-COLORDEPTH = 1000
-
-colormap = [0] * COLORDEPTH
-
-# some utility functions
-def constrain(val, min_val, max_val):
-    return min(max_val, max(min_val, val))
-
-
-def map_value(x, in_min, in_max, out_min, out_max):
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-
-
-def gaussian(x, a, b, c, d=0):
-    return a * math.exp(-((x - b) ** 2) / (2 * c ** 2)) + d
-
-
-def gradient(x, width, cmap, spread=1):
-    width = float(width)
-    r = sum(
-        [gaussian(x, p[1][0], p[0] * width, width / (spread * len(cmap))) for p in cmap]
-    )
-    g = sum(
-        [gaussian(x, p[1][1], p[0] * width, width / (spread * len(cmap))) for p in cmap]
-    )
-    b = sum(
-        [gaussian(x, p[1][2], p[0] * width, width / (spread * len(cmap))) for p in cmap]
-    )
-    r = int(constrain(r * 255, 0, 255))
-    g = int(constrain(g * 255, 0, 255))
-    b = int(constrain(b * 255, 0, 255))
-    return r, g, b
-
-
-for i in range(COLORDEPTH):
-    colormap[i] = gradient(i, COLORDEPTH, heatmap)
-
-
 
 # initialize the sensor
 mlx = adafruit_mlx90640.MLX90640(i2c)
@@ -93,16 +42,12 @@ except ValueError:
 
 pixels = [0] * 768
 for i, pixel in enumerate(frame):
-    coloridx = map_value(pixel, MINTEMP, MAXTEMP, 0, COLORDEPTH - 1)
-    coloridx = int(constrain(coloridx, 0, COLORDEPTH - 1))
-    pixels[i] = colormap[coloridx]
-
-for h in range(24):
-    for w in range(32):
-        pixel = pixels[h * 32 + w]
+    if pixel < MINTEMP:
+        pixels[i]=15
+    pixels[i] = int((pixel-MINTEMP)*(255/(MAXTEMP-MINTEMP)))
 
 # pixelrgb = [colors[constrain(int(pixel), 0, COLORDEPTH-1)] for pixel in pixels]
-img = Image.new("RGB", (32, 24))
+img = Image.new("L", (32, 24))
 img.putdata(pixels)
 img = img.resize((32 * INTERPOLATE, 24 * INTERPOLATE), Image.BICUBIC)
 img.save(r'termalnaslika.png')
