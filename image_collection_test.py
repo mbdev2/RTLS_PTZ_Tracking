98f0c931f7cd1a5d79c434ec162f16d732600f59
mbdev2 <mark.breznik@gmail.com>
2021-06-15 07:59:33 +0200

    Fixes

--- image_collection_test.py
+++ image_collection_test.py
@@ -16,7 +16,7 @@ INTERPOLATE = 10
 # MUST set I2C freq to 1MHz in /boot/config.txt
 i2c = busio.I2C(board.SCL, board.SDA)
 # low range of the sensor (this will be black on the screen)
-MINTEMP =20.0
+MINTEMP =22.0
 # high range of the sensor (this will be white on the screen)
 MAXTEMP = 36.0
 
@@ -32,27 +32,29 @@ def main():
     print("Refresh rate: ", pow(2, (mlx.refresh_rate - 1)), "Hz")
 
     for poskusi in range (0,600):
-         # capture thermal frame
-         frame = [0] * 768
-         while True:
-             try:
-                 mlx.getFrame(frame)
-                 break
-             except Exception:
-                 #in case we get a traceback error, we just retry the connection and go again, no biggie
-                 print(traceback.format_exc())
-                 mlx = adafruit_mlx90640.MLX90640(i2c)
-                 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
-                 continue
+        # capture thermal frame
+        frame = [0] * 768
+        while True:
+         try:
+             mlx.getFrame(frame)
+             break
+         except Exception:
+             #in case we get a traceback error, we just retry the connection and go again, no biggie
+             print(traceback.format_exc())
+             mlx = adafruit_mlx90640.MLX90640(i2c)
+             mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
+             continue
 
         #Limit the temp range between MINTEMP and MAXTEMP for higher accuracy
         pixels = [0] * 1024
-        for i in range(0,1024):
-            if i<128 || i>895:
+        for i in range(0,1023):
+            if i<128 or i>895:
                 pixels[i]=MINTEMP
-            else frame[i] < MINTEMP:
+            elif frame[i-128] < MINTEMP:
                 pixels[i]=MINTEMP
-            pixels[i] = int((pixel-MINTEMP)*(255/(MAXTEMP-MINTEMP)))
+            else:
+                pixels[i]=frame[i-128]
+            pixels[i] = int((pixels[i]-MINTEMP)*(255/(MAXTEMP-MINTEMP)))
 
         #use PIL library to interpolate by a factor of INTERPOLATE -> increasing resolution to 320x240 and smoothing out the mosaicing effect
         img2 = Image.new("L", (32, 32)) #the frame should actually be 32x24, but our Object Detection on Edge Impulse is limted to squares
@@ -61,10 +63,10 @@ def main():
         img= np.array(img2) #since CV uses numpy arrays for image manipulation, we convert our PIL image to an array
 
         #save images to disk
-        name="collection_"+str(poskusi)
+        name="/home/pi/Desktop/RTLS_FindMyProfessor/testing/pics/collection_"+str(poskusi)+".png"
         cv2.imwrite(name, img)
         print("Saved image ", poskusi)
 
 
 if __name__ == "__main__":
-main()
+    main()
